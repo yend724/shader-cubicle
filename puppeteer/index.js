95ad55e2895cfd70488150b447db4eff115fb6b3
yend724 <yend9524@gmail.com>
2022-08-14 19:03:39 +0900

    Add cos

--- puppeteer/index.js
+++ puppeteer/index.js
@@ -2,38 +2,44 @@ const fs = require("fs");
 const puppeteer = require("puppeteer-core");
 
 (async () => {
-  console.log("start");
+  console.log("Process start!");
   const pages = JSON.parse(fs.readFileSync("src/data/route.json", "utf8"));
   const route = pages["pages"].map(v => {
     return v.path.toLocaleLowerCase().replace(/\s+/g, "-");
   });
 
   const browser = await puppeteer.launch({
-    headless: true, // フルバージョンのChromeを使用
+    headless: false,
     // slowMo: 300,
     executablePath:
       "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome",
   });
   const page = await browser.newPage();
-  for (let i = 0; i < route.length; i++) {
-    await page.goto(`http://localhost:3000/code/${route[i]}`);
-    console.log(`open: http://localhost:3000/code/${route[i]}`);
-    await page.setViewport({ width: 800, height: 800 });
-    await page.waitForSelector("#webgl");
-    await page.evaluate(() => {
-      document.body.style.backgroundColor = "#000";
-      const header = document.getElementById("header");
-      if (header) {
-        header.style.opacity = 0;
-      }
-      const webgl = document.getElementById("webgl");
-      if (webgl) {
-        window.scrollTo(0, webgl.getBoundingClientRect().top + 1 - 32);
-      }
-    });
-    await page.screenshot({ path: `public/images/code/${route[i]}.png` });
-    console.log("screenshot: " + route[i]);
+  try {
+    for (let i = 0; i < route.length; i++) {
+      await page.goto(`http://localhost:3000/code/${route[i]}`);
+      console.log(`open: http://localhost:3000/code/${route[i]}`);
+      await page.setViewport({ width: 800, height: 800 });
+      await page.waitForSelector("#webgl");
+      await page.evaluate(() => {
+        document.body.style.backgroundColor = "#000";
+        const header = document.getElementById("header");
+        if (header) {
+          header.style.opacity = 0;
+        }
+        const webgl = document.getElementById("webgl");
+        if (webgl) {
+          window.scrollTo(0, webgl.getBoundingClientRect().top + 1 - 32);
+        }
+      });
+      await page.screenshot({ path: `public/images/code/${route[i]}.png` });
+      console.log("screenshot: " + route[i]);
+    }
+    console.log("All Success!");
+    await browser.close();
+  } catch (error) {
+    console.log("error: " + error);
+  } finally {
+    await browser.close();
   }
-  console.log("All Success!");
-  await browser.close();
 })();
