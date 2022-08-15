fd53de27ba09742da38723741d4b1933391ddce9
yend724 <yend9524@gmail.com>
2022-08-15 21:40:09 +0900

    Update Head

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import glsl from "vite-plugin-glsl";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://shader-cubicle.yend.dev",
+  trailingSlash: "always",
   vite: {
     plugins: [glsl()],
   },
