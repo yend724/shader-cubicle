a3c6eacaf01ff1b60174a2208ec2e615dd0e7dff
yend724 <yend9524@gmail.com>
2022-08-15 16:42:44 +0900

    Update circle

--- src/assets/js/circle/shaders/fragment.glsl
+++ src/assets/js/circle/shaders/fragment.glsl
@@ -1,7 +1,9 @@
 varying vec2 vUv;
 
 void main () {
-  vec2 center = vec2(0.5, 0.5);
-  float g = distance(vUv, center) < 0.5 ? 1.0: 0.0;
-  gl_FragColor = vec4(0.0, g, 0.0, 1.0);
+  vec2 center = vec2(0.4);
+  float radius = 0.2;
+  float circle = 1.0 - step(radius, distance(center, vUv));
+  vec3 color = vec3(0., 1.0, 1.0) * circle;
+  gl_FragColor = vec4(color, 1.0);
 }
