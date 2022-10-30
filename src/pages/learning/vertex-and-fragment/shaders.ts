beb40c90d1dc5abed9c5ddd5dbff8abcf11235d3
yend724 <yend9524@gmail.com>
2022-10-31 01:30:12 +0900

    Update articles

--- src/pages/learning/vertex-and-fragment/shaders.ts
+++ src/pages/learning/vertex-and-fragment/shaders.ts
@@ -1,21 +1,19 @@
-export const vertexShader = `varying vec2 vUv;
-
-void main () {
-  vUv = uv;
-  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);
+export const vertexShader = `void main () {
+  vec3 myPosition = position;
+  myPosition.y += cos(myPosition.x); // オブジェクトのY座標をconで歪める
+  vec4 modelPosition = modelMatrix * vec4(myPosition, 1.0);
+  modelPosition.z += -1.0; // オブジェクトの位置をZ軸方向に-1
+  modelPosition.y += -1.0; // オブジェクトの位置をY軸方向に-1
+  gl_Position = projectionMatrix * viewMatrix * modelPosition;
 }`;
-export const fragmentShader = `varying vec2 vUv;
-
-void main () {
-  // (R, G, B) = (1.0, 1.0, 1.0) なので白
-  vec3 white = vec3(1.0, 1.0, 1.0);
-
-  // (R, G, B) = (0.0, 0.0, 0.0) なので黒
-  vec3 black = vec3(0.0, 0.0, 0.0);
-
-  // UV座標のX軸を基準に線形補間
-  // vUx.x は 0 ~ 1 の間
-  vec3 mixed = mix(white, black, vUv.x);
-
-  gl_FragColor = vec4(mixed, 1.0);
+export const fragmentShader = `void main () {
+  gl_FragColor = vec4(0.0, 0.0, 0.0, 1.0);
+}`;
+export const vertexShader02 = `void main () {
+  gl_Position = projectionMatrix * viewMatrix * modelMatrix * vec4(position, 1.0);;
+}`;
+export const fragmentShader02 = `void main () {
+  // (赤、緑、青、透過度) = (1.0, 0.0, 0.0, 1.0);
+  vec4 rgba = vec4(1.0, 0.0, 0.0, 1.0);
+  gl_FragColor = vec4(rgba);
 }`;
